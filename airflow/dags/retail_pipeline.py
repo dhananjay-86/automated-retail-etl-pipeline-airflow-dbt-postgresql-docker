0fff73b89ad8d21bafb72f134d2e45024b1ee416
dhananjay-86 <dhananjaykatre86@gmail.com>
2026-07-27 12:49:43 +0530

    Implement production-ready Airflow ETL pipeline

--- airflow/dags/retail_pipeline.py
+++ airflow/dags/retail_pipeline.py
@@ -1,37 +1,49 @@
 from datetime import datetime
+import sys
+
+sys.path.append("/opt/project")
+
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from python.database.init_database import initialize_database
+from python.etl.load_customers import load_customers
+from python.etl.load_products import load_products
+from python.etl.load_orders import load_orders
+
 
 with DAG(
     dag_id="retail_etl_pipeline",
     start_date=datetime(2026, 1, 1),
     schedule="@daily",
     catchup=False,
-    tags=["retail", "etl"],
+    tags=["Retail", "ETL"],
 ) as dag:
 
-    start = EmptyOperator(
-        task_id="start"
+    start = EmptyOperator(task_id="start")
+
+    initialize_db = PythonOperator(
+        task_id="initialize_database",
+        python_callable=initialize_database,
     )
 
-    python_etl = BashOperator(
-        task_id="python_etl",
-        bash_command="echo 'Running Python ETL...'"
+    customers = PythonOperator(
+        task_id="load_customers",
+        python_callable=load_customers,
     )
 
-    dbt_run = BashOperator(
-        task_id="dbt_run",
-        bash_command="echo 'Running dbt run...'"
+    products = PythonOperator(
+        task_id="load_products",
+        python_callable=load_products,
     )
 
-    dbt_test = BashOperator(
-        task_id="dbt_test",
-        bash_command="echo 'Running dbt test...'"
+    orders = PythonOperator(
+        task_id="load_orders",
+        python_callable=load_orders,
     )
 
-    end = EmptyOperator(
-        task_id="end"
-    )
+    end = EmptyOperator(task_id="end")
 
-    start >> python_etl >> dbt_run >> dbt_test >> end
+    start >> initialize_db >> customers >> products >> orders >> end
+    
